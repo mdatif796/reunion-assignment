f9c3706282e4c03c9386b57bfe286b40fcc0c9ca
mdatif796 <mdatif796@gmail.com>
2023-03-16 16:17:49 +0530

    connected user and post schema with comment schema

--- models/comment.js
+++ models/comment.js
@@ -6,6 +6,14 @@ const commentSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
   },
   {
     timestamps: true,
