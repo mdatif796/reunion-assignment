8b94ef3ffd43481f06d3d26a74bf49db84c0199c
mdatif796 <mdatif796@gmail.com>
2023-03-16 17:05:11 +0530

    created route and controller for autheticate user

--- controllers/api/userController.js
+++ controllers/api/userController.js
@@ -1,5 +1,6 @@
-const User = require("../../models/user");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const User = require("../../models/user");
 
 module.exports.home = (req, res) => {
   return res.status(200).json({
@@ -7,6 +8,7 @@ module.exports.home = (req, res) => {
   });
 };
 
+// create user
 module.exports.createUser = async (req, res) => {
   try {
     // first check whether user exist or not
@@ -28,6 +30,37 @@ module.exports.createUser = async (req, res) => {
       message: "User created!!",
       user,
     });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+// authenticate user
+module.exports.authenticateUser = async (req, res) => {
+  try {
+    // if email and password is not undefined
+    if (req.body.email && req.body.password) {
+      // check whether it exist or not
+      let user = await User.findOne({ email: req.body.email });
+      // user exist and user password matches with the login credentials
+      if (user && user.password === req.body.password) {
+        return res.status(201).json({
+          success: true,
+          message: "successfully authenticated the user",
+          jwt_token: {
+            token: jwt.sign(user.toJSON(), process.env.JWTSECRETKEY),
+          },
+        });
+      }
+      // if not matched
+      return res.status(401).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
   } catch (error) {
     return res.status(500).json({
       success: false,
--- routes/api/index.js
+++ routes/api/index.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const userController = require("../../controllers/api/userController");
 // create user
 router.post("/create-user", userController.createUser);
+// authenticate user
+router.post("/authenticate", userController.authenticateUser);
 
 module.exports = router;
